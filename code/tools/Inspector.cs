af9ff55114b82d641faf90b49de5c2ee5e184115
Junior <sudojunior@outlook.com>
2022-08-05 23:35:25 +0100

    Changeset of 6 - enhanced persistence * Allow persistence after reactivating * Extract particle creation to its own method * Add secondary action to remove current debug target * World is no longer targeted or accepted as part of input * Frame event shows debug text on the selected prop with WorldText and at the side with ScreenText * HitEffects and particle creation were moved further up before input decisions are acted on

--- code/tools/Inspector.cs
+++ code/tools/Inspector.cs
@@ -1,8 +1,23 @@
-﻿namespace Sandbox.Tools
+﻿using System.Linq;
+
+namespace Sandbox.Tools
 {
 	[Library( "tool_inspector", Title = "Inspector", Description = "Inspects entities", Group = "construction" )]
 	public partial class InspectorTool : BaseTool
 	{
+		[Net] private Entity Selected { get; set; } = null;
+
+		public override void Activate()
+		{
+			base.Activate();
+			if (Host.IsServer)
+			{
+				// Selected = null;
+			}
+		}
+
+		private Particles CreateParticles() => Particles.Create( "particles/physgun_freeze.vpcf" );
+
 		public override void Simulate()
 		{
 			if ( !Host.IsServer )
@@ -10,27 +25,50 @@
 
 			using ( Prediction.Off() )
 			{
-				if ( !Input.Pressed( InputButton.PrimaryAttack ) )
+				if ( !Input.Pressed( InputButton.PrimaryAttack ) && !Input.Pressed( InputButton.SecondaryAttack ) )
 					return;
 
 				var tr = DoTrace();
 
-				if ( !tr.Hit || !tr.Entity.IsValid() )
+				if ( !tr.Hit || !tr.Entity.IsValid() || tr.Entity.IsWorld )
 					return;
+
+				CreateHitEffects( tr.EndPosition );
+				CreateParticles().SetPosition( 0, tr.Entity.Position );
+
+				if ( Input.Pressed(InputButton.SecondaryAttack) )
+				{
+					if ( Selected != tr.Entity ) return;
+
+					Selected = null;
+
+					return;
+				}
 
 				// if ( tr.Entity is Player )
 				//	return;
 
-				CreateHitEffects( tr.EndPosition );
-
-				if ( tr.Entity.IsWorld )
-					return;
-
 				// tr.Entity.Delete();
-				Log.Info( tr.Entity );
+				Selected = tr.Entity;
+			}
+		}
 
-				var particle = Particles.Create( "particles/physgun_freeze.vpcf" );
-				particle.SetPosition( 0, tr.Entity.Position );
+		[Event.Frame]
+		public override void OnFrame()
+		{
+			base.OnFrame();
+			if (Selected != null)
+			{
+				if (!Selected.IsValid()) {
+					Selected = null;
+					return;
+				}
+
+				DebugOverlay.Text( Selected.Name, Selected.Position );
+
+				DebugOverlay.ScreenText( Selected.Name, 3 );
+				DebugOverlay.ScreenText( $"Position: {Selected.Position}", 4 );
+				DebugOverlay.ScreenText( $"Rotation: {Selected.Rotation}", 5 );
 			}
 		}
 	}
